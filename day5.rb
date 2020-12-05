c2a8cce2b9a023fd0601e1b75932b29f7a82d074
andrewchang <andrewc@hotdoc.com.au>
2020-12-05 12:13:13 +1100

    day 5

--- day5.rb
+++ day5.rb
@@ -33,3 +33,44 @@ end
 
 puts "#{solve(input)}"
 
+# Part 2
+
+input = File.read("input.txt").split("\n").map do |i|
+  i
+end
+
+def solve(input)
+  ids = []
+  input.each do |i|
+    row_code = i[0..6]
+    col_code = i[7..9]
+    row = [0, 127]
+    col = [0, 7]
+
+    row_code.each_char do |i|
+      row = half(i, row)
+    end
+
+    col_code.each_char do |i|
+      col = half(i, col)
+    end
+
+    ids.append(row[0]*8 + col[0])
+  end
+  sorted = ids.sort
+  sorted.each_with_index do |i, idx|
+    if i + 1 != sorted[idx + 1]
+      return i + 1
+    end
+  end
+end
+
+def half(letter, current_half)
+  if letter == 'F' || letter == 'L'
+    return [current_half[0],  ((current_half[1] - 1) - current_half[0])/2 + current_half[0]]
+  elsif letter == 'B' || letter == 'R'
+    return [((current_half[1] + 1) - current_half[0])/2 + current_half[0], current_half[1]]
+  end
+end
+
+puts "#{solve(input)}
