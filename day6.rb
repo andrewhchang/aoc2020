190cd445c5dfa7601eead90d56ae5de72cd96317
andrewchang <andrewc@hotdoc.com.au>
2020-12-07 14:47:04 +1100

    day 6

--- day6.rb
+++ day6.rb
@@ -7,3 +7,18 @@ def solve(input)
 end
 
 puts "#{solve(input)}}
+
+# part 2
+
+input = File.read("input.txt").split("\n\n").map do |i|
+  i.split("\n").map { |j| j.split(//) }
+end
+
+def solve(input)
+  ans = input.map do |i|
+    i.inject { |j, k| j & k}.length
+  end
+  ans.reduce(:+)
+end
+
+puts "#{solve(input)}"
